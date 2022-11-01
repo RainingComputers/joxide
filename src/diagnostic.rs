855b00f2f4ca9e48e3e9d5f26e4c3a420d96417f
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-11-01 18:42:27 +0530

    Change to_owned to to_string

--- src/diagnostic.rs
+++ src/diagnostic.rs
@@ -5,7 +5,7 @@ use crate::{
 
 fn get_message_unexpected_token<'a>(parse_error: &'a ParseError<'a>) -> String {
     match parse_error.token {
-        None => "Did not expect this word or character".to_owned(),
+        None => "Did not expect this word or character".to_string(),
         Some(token) => match parse_error.expected {
             None => format!("Did not expect '{}'", token.token_type),
             Some(expected) => match expected {
@@ -24,11 +24,11 @@ fn get_message_unexpected_token<'a>(parse_error: &'a ParseError<'a>) -> String {
 
 pub fn get_message<'a>(parse_error: &'a ParseError<'a>) -> String {
     match parse_error.error_type {
-        ParseErrorType::UnexpectedEnd => "File ended unexpectedly".to_owned(),
+        ParseErrorType::UnexpectedEnd => "File ended unexpectedly".to_string(),
         ParseErrorType::UnexpectedToken => get_message_unexpected_token(parse_error),
-        ParseErrorType::TrailingComma => "Trailing commas are not valid".to_owned(),
-        ParseErrorType::DuplicateKey => "Duplicate keys are not valid".to_owned(),
-        ParseErrorType::KeyNotInQuotes => "Key should be in quotes".to_owned(),
-        ParseErrorType::MissingColon => "Missing a ':' separator".to_owned(),
+        ParseErrorType::TrailingComma => "Trailing commas are not valid".to_string(),
+        ParseErrorType::DuplicateKey => "Duplicate keys are not valid".to_string(),
+        ParseErrorType::KeyNotInQuotes => "Key should be in quotes".to_string(),
+        ParseErrorType::MissingColon => "Missing a ':' separator".to_string(),
     }
 }
