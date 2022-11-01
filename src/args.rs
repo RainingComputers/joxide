14c5374a6a15a1da586227194741187197ea438f
RainingComputers <vishnu.vish.shankar@gmail.com>
2022-11-01 22:46:36 +0530

    Added CLI args

---
+++ src/args.rs
@@ -0,0 +1,41 @@
+use argh::FromArgs;
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// joxide, CLI tool for formatting and validating JSON files
+pub struct JoxideArgs {
+    #[argh(subcommand)]
+    pub sub_command: JoxideSubcommand,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand)]
+pub enum JoxideSubcommand {
+    Format(FormatArgs),
+    Validate(ValidateArgs),
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// format json file
+#[argh(subcommand, name = "format")]
+pub struct FormatArgs {
+    #[argh(option, default = "4")]
+    /// indent length, default is 4
+    pub indent_length: usize,
+
+    #[argh(option, default = "false")]
+    /// modify the file instead of printing to console
+    pub write: bool,
+
+    #[argh(positional)]
+    /// path to the file you want to format
+    pub file: String,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+/// validate json file for syntax errors
+#[argh(subcommand, name = "validate")]
+pub struct ValidateArgs {
+    #[argh(positional)]
+    /// path to the file you want to validate
+    pub file: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use std::{env, process::ExitCode};
+extern crate argh;
+use crate::args::JoxideSubcommand;
+use std::process::ExitCode;
 
+mod args;
 mod diagnostic;
 mod formatter;
 mod lexer;
@@ -7,16 +10,14 @@ mod parser;
 mod pretty;
 
 fn main() -> ExitCode {
-    let args: Vec<String> = env::args().collect();
-    let file_path = match args.get(1) {
-        Some(path) => path,
-        None => {
-            println!("Invalid CLI arguments");
-            return ExitCode::FAILURE;
-        }
+    let args: args::JoxideArgs = argh::from_env();
+
+    let file_path = match args.sub_command {
+        JoxideSubcommand::Validate(ref validate_args) => &validate_args.file,
+        JoxideSubcommand::Format(ref format_args) => &format_args.file,
     };
 
-    let raw = match std::fs::read_to_string(file_path) {
+    let raw = match std::fs::read_to_string(&file_path) {
         Ok(content) => content,
         Err(err) => {
             println!("Unable to open file, reason: {}", err.to_string());
@@ -25,6 +26,7 @@ fn main() -> ExitCode {
     };
 
     let tokens = lexer::lex(&raw);
+
     let value = match parser::parse(&tokens) {
         Ok(value) => value,
         Err(parse_error) => {
@@ -38,7 +40,18 @@ fn main() -> ExitCode {
         }
     };
 
-    println!("{}", formatter::format_json(value, 4));
+    if let JoxideSubcommand::Format(ref format_args) = args.sub_command {
+        let formatted = formatter::format_json(value, format_args.indent_length);
+
+        if format_args.write {
+            if let Err(err) = std::fs::write(file_path, formatted) {
+                println!("Unable to write to file, reason: {}", err.to_string());
+                return ExitCode::FAILURE;
+            }
+        } else {
+            println!("{}", formatted);
+        }
+    }
 
     return ExitCode::SUCCESS;
 }
